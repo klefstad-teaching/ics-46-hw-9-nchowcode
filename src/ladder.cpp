ae9cb03b70dcb9918eb20b801548000abf3268be
Nathan <chowns@uci.edu>
2025-03-16 01:04:51 -0700

    fix: erorr logs

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -118,6 +118,11 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 
 void print_word_ladder(const vector<string>& ladder) {
+    if (ladder.empty()) {
+        cout << "No word ladder found." << endl;
+        return;
+    }
+
     cout << "Word ladder found: ";
     for (const string& word : ladder) {
         cout << word << " ";
